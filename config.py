c60f7eb064e038b8f6ef4b8e5f02cb1278356696
Oddvar Folkestad <hanev@online.no>
2026-01-11 17:43:57 +0100

    fix(Config) added more var

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
+
 class Config:    
     def __init__(self):
         self.num_epochs = 100
         self.num_timesteps = 10
-        self.learning_rate = 0.001
+        self.learning_rate = 0.01
+        self.noise_range = (-0.03, 0.03)
         
-        self.noise_range = (-0.03, 0.03)  
+        self.bathtub_area = 10
+        self.drain_area = 0.3
+        self.initial_height = 5
+               
+        # Classic PID start-verdier
+        self.pid_kp_init = 0.04
+        self.pid_ki_init = 1
+        self.pid_kd_init = 0.1
         
-        self.bathtub_area = 1.0
-        self.drain_area = 0.02      
-        self.initial_height = 1.0
-        
+        self.nn_layers = [3, 8, 8, 1]  
+        self.nn_activation = 'tanh'     # 'tanh', 'sigmoid', eller 'relu'
