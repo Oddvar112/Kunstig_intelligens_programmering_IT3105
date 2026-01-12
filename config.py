c06bb482baf4730e8eb3b949e965ba620e3865b9
Oddvar Folkestad <hanev@online.no>
2026-01-12 16:41:45 +0100

    fix(Config) update learning rate and PID controller initial values

--- config.py
+++ config.py
@@ -1,19 +1,25 @@
-
 class Config:    
     def __init__(self):
         self.num_epochs = 100
         self.num_timesteps = 10
-        self.learning_rate = 0.1
-        self.noise_range = (-0.03, 0.03)
+        self.learning_rate = 0.001
+        self.noise_range = (-0.01, 0.01)
+        
+        self.plant_type = "cournot"  # "bathtub" eller "cournot"
         
         self.bathtub_area = 10
         self.drain_area = 0.3
         self.initial_height = 5
+        
+        self.pmax = 4              # Maximum price
+        self.cm = 0.1                # Marginal cost
+        self.target_profit = 2     # Target profit per timestep
+        self.q1_init = 0.5           # Initial production (oss)
+        self.q2_init = 0.5           # Initial production (rival)
                
-        # Classic PID start-verdier
-        self.pid_kp_init = 1
-        self.pid_ki_init = 1
-        self.pid_kd_init = 1
+        self.pid_kp_init = 0
+        self.pid_ki_init = 0
+        self.pid_kd_init = 0
         
         self.nn_layers = [3, 8, 8, 1]  
-        self.nn_activation = 'tanh'     # 'tanh', 'sigmoid', eller 'relu'
+        self.nn_activation = 'relu'     # 'tanh', 'sigmoid', eller 'relu'
