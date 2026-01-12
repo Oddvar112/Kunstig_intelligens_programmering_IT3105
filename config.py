d0f790eaf6d2906b42e625ce864f8080b6760ece
Oddvar Folkestad <hanev@online.no>
2026-01-12 15:03:54 +0100

    fix(Config) update learning rate and PID controller initial values

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     def __init__(self):
         self.num_epochs = 100
         self.num_timesteps = 10
-        self.learning_rate = 0.01
+        self.learning_rate = 0.1
         self.noise_range = (-0.03, 0.03)
         
         self.bathtub_area = 10
@@ -11,9 +11,9 @@ class Config:
         self.initial_height = 5
                
         # Classic PID start-verdier
-        self.pid_kp_init = 0.04
+        self.pid_kp_init = 1
         self.pid_ki_init = 1
-        self.pid_kd_init = 0.1
+        self.pid_kd_init = 1
         
         self.nn_layers = [3, 8, 8, 1]  
         self.nn_activation = 'tanh'     # 'tanh', 'sigmoid', eller 'relu'
