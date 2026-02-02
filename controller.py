e53fe0387432963a86ff8de971851dcd0d636b61
Oddvar Folkestad <hanev@online.no>
2026-02-02 14:18:59 +0100

    feat(Controller) add abstract base class for controller interface

---
+++ controller.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Controller(ABC):
+    
+    @abstractmethod
+    def get_params(self):
+        pass
+    
+    @abstractmethod
+    def set_params(self, params):
+        pass
+    
+    @abstractmethod
+    def compute_control(self, error_history):
+        pass
+    
+    @abstractmethod
+    def update_params(self, params, grads, learning_rate):
+        pass
